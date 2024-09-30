8525ce86fbcb39e59e940d6beceef52497a45681
DavidRivera011 <62643408+DavidRivera011@users.noreply.github.com>
2024-09-30 01:38:59 -0600

    Add files via upload

---
+++ SUCURSALES/src/main/java/com/example/sucursales/Empleado.java
@@ -0,0 +1,104 @@
+package com.example.sucursales;
+
+public class Empleado {
+    private String id;
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String direccion;
+    private String categoriaLaboral;
+    private double salario;
+    private String correoElectronico;
+    private String fechaNacimiento;
+
+    // Constructor
+    public Empleado() {
+    }
+
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCategoriaLaboral() {
+        return categoriaLaboral;
+    }
+
+    public void setCategoriaLaboral(String categoriaLaboral) {
+        this.categoriaLaboral = categoriaLaboral;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return primerNombre + " " + primerApellido; // Representación opcional
+    }
+}
